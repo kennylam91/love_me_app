aac4ea10afeb311b69586ae0ed9efd60811f10e7
kennylam91 <phamvanlam1991@gmail.com>
2019-04-27 22:35:11 +0700

    add transition

--- love_me_app.js
+++ love_me_app.js
@@ -1,17 +1,17 @@
-function yesfunction(){
-let Obj=document.getElementById("iloveyou");
-Obj.style.display="block";
+function yesfunction() {
+    let Obj = document.getElementById("iloveyou");
+    Obj.style.display = "block";
 }
-function nofunction(){
-    let Obj=document.getElementById("no");
-    Obj.style.position="absolute";
-    Obj.style.margin="0px";
-    let x=(window.innerWidth-10);
-    let y=(window.innerHeight-10);
-    let r=Math.random();
-    Obj.style.left=eval(x*r)+"px";
-    Obj.style.top=eval(y*r)+"px";
-
-
-
+function nofunction() {
+    let Obj = document.getElementById("no");
+    Obj.style.position = "absolute";
+    Obj.style.margin = "0px";
+    let x = (window.innerWidth - 10);
+    let y = (window.innerHeight - 10);
+    let xrandom = Math.random();
+    let yrandom = Math.random();
+    Obj.style.left = eval(xrandom * x) + "px";
+    Obj.style.top = eval(yrandom * y) + "px";
+    console.log(xrandom);
+    console.log(yrandom);
 }
